21d8f32886818e61a4c6275f534eaabbdb012518
Prathamesh Gujar <gujarpa@rknec.edu>
2022-10-27 09:40:49 +0530

    Add files via upload

---
+++ Sorting-Algorithms-Visualizer/Algorithms/Insertion_Sort.py
@@ -0,0 +1,17 @@
+# importing time module to create some time difference between each comparison while sorting
+import time
+
+# import colors from colors.py
+from Helper.color import *
+
+def insertion_sort(data, drawData, timeTick):
+    for i in range(len(data)):
+        temp = data[i]
+        k = i
+        while k > 0 and temp < data[k - 1]:
+            data[k] = data[k - 1]; k -= 1
+        data[k] = temp
+        drawData(data, [YELLOW if x == k or x == i else BLUE for x in range(len(data))])
+        time.sleep(timeTick)
+        
+    drawData(data, [BLUE for x in range(len(data))])
